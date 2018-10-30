a92e3e880637f93c1f7483ca7f4a74a88288f610
Matt <matt@addlestone.us>
2018-10-30 16:24:26 -0400

    [CHANGED] added voc2yolo3[copy from darknet]

--- darknet/darknet/voc_label.py
+++ darknet/darknet/voc_label.py
@@ -6,14 +6,13 @@ from shutil import copyfile
 from os import listdir, getcwd
 from os.path import join
 
+
 sets=[('2012', 'train'), ('2012', 'val')]
 
-classes_name = ["face"]
+classes_name = ["face","bicycle","bus", "car",  "motorbike", "person"]
 
 
 def convert(size, box):
-    if len(size) != 4 or len(box) != 2:
-        return
     dw = 1./size[0]
     dh = 1./size[1]
     x = (box[0] + box[1])/2.0
@@ -61,9 +60,6 @@ def trans_dlib_2_yolo3(treenode,outfile,infoler,outfolder,clsname):
         imgid = img.get('file')
         imgfilebasename = os.path.basename(imgid)
         imgfilename, imgextension = os.path.splitext(imgfilebasename)
-
-
-        #tidy filename
         imgfilename = imgfilename.replace("jpg",'_')
         imgfilename = imgfilename.replace("JPG", '_')
         imgfilename = imgfilename.replace("jpeg", '_')
@@ -100,6 +96,45 @@ def process_dlib_2_yolo3(infoler,outfolder):
     trans_dlib_2_yolo3(tree_test,outfolder+"/test.txt",infoler,outfolder,"face")
 
 
+
+
+def convert_annotation_voc_yolo3(infoler,outfolder,year, image_id):
+    in_file = open('%s/VOC%s/Annotations/%s.xml'%(infoler,year, image_id))
+    out_file = open('%s/VOC%s/labels/%s.txt'%(infoler,year, image_id), 'w')
+    tree = ET.parse(in_file)
+    root = tree.getroot()
+    size = root.find('size')
+    w = int(size.find('width').text)
+    h = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls = obj.find('name').text
+        if cls not in classes_name or int(difficult) == 1:
+            continue
+        cls_id = classes_name.index(cls)
+        xmlbox = obj.find('bndbox')
+        b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        bb = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+
+def trans_voc_2_yolo3(infoler,outfolder):
+    for year, image_set in sets:
+        if not os.path.exists('%s/VOC%s/labels/' % (infoler,year)):
+            os.makedirs('%s/VOC%s/labels/' % (infoler,year))
+        image_ids = open('%s/VOC%s/ImageSets/Main/%s.txt' % (infoler,year, image_set)).read().strip().split()
+        list_file = open('%s/%s_%s.txt' % (outfolder,year, image_set), 'w')
+        for image_id in image_ids:
+            list_file.write('%s/VOC%s/JPEGImages/%s.jpg\n' % (infoler, year, image_id))
+            convert_annotation_voc_yolo3(infoler,outfolder,year, image_id)
+        list_file.close()
+
+def process_voc_2_yolo3(infoler,outfolder):
+    trans_voc_2_yolo3(infoler,outfolder)
+
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print('Usage: dataset_formatt[dlib2yolo3] dataset_folder output_folder')
@@ -111,5 +146,6 @@ if __name__ == '__main__':
 
     if g_format == "dlib2yolo3":
         process_dlib_2_yolo3(g_input,g_output)
-    else:
+    if g_format == "voc2yolo3":
+        process_voc_2_yolo3(g_input, g_output)
         print('wrong format[dlib2yolo3]')
